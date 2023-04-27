8cc83ffe1e9b307290f28d198d6183a7dcd4f861
Husayn <hussainiahmed222@gmail.com>
2023-04-27 11:46:28 +0100

    Update BMI_Calculator.py

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -5,6 +5,19 @@ new_Height = float(height)
 new_Weight = int(weight)
 
 bmi = new_Weight / new_Height ** 2
-new_Bmi = int(bmi)
-print(new_Bmi)
- 
+new_Bmi = round(bmi)
+if new_Bmi <= 18.5:
+    result = "are underweight"
+    print(f"Your BMI is {new_Bmi} and you {result}")
+elif new_Bmi < 25:
+    result = "have anormal weight"
+    print(f"Your BMI is {new_Bmi} and you {result}")
+elif new_Bmi < 30:
+    result = "are overweight"
+    print(f"Your BMI is {new_Bmi} and you {result}")
+elif new_Bmi < 35:
+    result = "are obese"
+    print(f"Your BMI is {new_Bmi} and you {result}")
+else:
+    result = "are clinically obese"
+    print(f"Your BMI is {new_Bmi} and you {result}")
